bc40596e99a08db008b73926fcd27119f69147dc
OsbornDelAngel <ozdelangel@gmail.com>
2021-09-22 05:58:07 -0700

    lab 1 complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer.js';
+import Header from './Header.js';
+import Main from './Main.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h2>This is the app page</h2>
+    <Header />
+    <Main />
+    <Footer />
+    </>
   );
 }
 
