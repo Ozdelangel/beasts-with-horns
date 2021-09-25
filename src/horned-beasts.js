624359c098e7bf3a68cbef2679fc18fa7accc325
OsbornDelAngel <ozdelangel@gmail.com>
2021-09-25 01:20:00 -0700

    lab2done

--- src/horned-beasts.js
+++ src/horned-beasts.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import './hornedbeast.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeasts extends React.Component{
+    // have to add state
+    constructor(props){
+        super(props);
+        // state must be an object
+        this.state = {
+            numClicks: 0,
+            isFav: false,
+        }
+       
+    }
+    onAdd = () => {
+        this.setState({numClicks: this.state.numClicks + 1});
+        // console.log('button pressed');
+    }
+
+    notFav = () => {
+        if(this.state.numClicks > 0)
+        this.setState({numClicks:this.state.numClicks - 1});
+    }
+    setFav = () => {
+        if(this.state.isFav){
+            this.setState({isFav:false})
+        }
+        else{
+            this.setState({isFav:true})
+
+        }
+    }
     render() {
         return (
-            <>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.imageUrl} alt="a box" title=" a placeholder"/>
-            <p>{this.props.description}</p>
-            </>
+            
+            
+            
+            
+
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.props.image} alt="a box" title=" a beast" />
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+    {this.props.description}
+    </Card.Text>
+    <p onClick={this.setFav}>Number Of Clicks: {this.state.numClicks}</p>
+            {/* (a ternary is a if/else all wrapped in on) */}
+            <p>{(this.state.isFav) ? 'heart': ''}</p>
+    <Button className='m-1' onClick={this.onAdd}>Add Fav</Button>
+    <Button className='m-1'onClick={this.notFav}>remove</Button>
+  </Card.Body>
+</Card>
+            
         )
     }
 }
