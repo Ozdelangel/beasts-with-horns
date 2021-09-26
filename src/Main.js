a1ce3d90bc096ce3cd1f376abcb16731eb2cdde4
OsbornDelAngel <ozdelangel@gmail.com>
2021-09-25 23:50:37 -0700

    start of lab 3

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import HornedBeasts from './horned-beasts.js';
 import data from './data.json';
+
 class Main extends React.Component{
     //set functions
     render() {
         // set variables
-        let newArr = [];
         
-        data.forEach((horn,idx) => {
-            newArr.push(
-                <HornedBeasts key={idx} title={horn.title} image={horn.image_url} description={horn.description} />
-            )
-        });
+        
+        let newArr = data.map((horn,idx) => 
+            
+                <HornedBeasts key={idx} 
+                title={horn.title} 
+                image={horn.image_url} 
+                description={horn.description} 
+                toggleModal={this.props.toggleModal}/>
+            
+        );
         return (
             <>
             <h2> rate the horned Horned Beasts</h2>
+            
             {newArr};
+            
             </>
         )
     }
